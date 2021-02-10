5a533c7ea2485f6899cad56b2f51f7fa071cf8f7
emirxmertoglu <emirxmertoglu@outlook.com>
2021-02-10 06:26:21 +0300

    modified default vue components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <div id="nav"><router-link to="/">Home</router-link> |</div>
+  <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -22,9 +20,17 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+#nav a:hover {
+  background: crimson;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #fff;
+  background: #1565c0;
 }
 </style>
