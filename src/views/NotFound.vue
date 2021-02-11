f8326cb0ab86981eac2dc1ee59b5a775d530a6e7
emirxmertoglu <emirxmertoglu@outlook.com>
2021-02-11 22:42:38 +0300

    added not found page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="not-found">
+    <h2>404</h2>
+    <h3>Page not found</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style>
+.not-found {
+  margin-top: 50px;
+}
+</style>
